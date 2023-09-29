1a7e6ce49f66b0265097ea9036e3cd9804258f3b
Saurabh <saurabh3896@gmail.com>
2023-09-29 19:22:03 +0530

    refactor: moved to code to main file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
+struct Person {
+    name: String,
+    age: usize,
+}
+
+struct Book {
+    title: String,
+    author: String,
+    is_available: bool,
+}
+
+struct Library {
+    books: Vec<Book>,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let person = Person { name: "Billy".to_owned(), age: 21 };
+
+    let best_book = Book {
+        title: "Designing Data-Intensive Applications".to_owned(),
+        author: "Martin Kleppmann".to_owned(),
+        is_available: true,
+    };
+
+    let mut library = Library {
+        books: vec![]
+    };
+
+    library.books.push(best_book);
+
 }
